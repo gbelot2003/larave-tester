0b778f628ef9a018d625cbadca8f0ff2cad94370
Gerardo Belot <gerardo.belot@gmail.com>
2023-02-12 16:56:06 -0600

    configurando inMemory db para test

---
+++ tests/Feature/Users/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Test\Feature\Users;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function user_page_can_be_render()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $response = $this->get('/users')
+        ->assertOk();
+    }
+}
